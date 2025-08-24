90004522717e3978aedc87153012ebefa31a0650
Andrew Dorchak <Andrew.Dorchak98@gmail.com>
2025-08-24 11:49:39 -0700

    docker tweaks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -64,7 +64,7 @@ function App() {
             <img src={reactLogo} className="logo react" alt="React logo" />
           </a>
         </div>
-        <h1>Vite + React + whales</h1>
+        <h1>Vite + React + Whales</h1>
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
